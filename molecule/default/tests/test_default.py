d690b0d6b265709a2dcc5f2f38eff941dd225416
Ali Muhammad <40025815+darkwizard242@users.noreply.github.com>
2020-01-26 00:28:15 -0500

    set assertion for molecule testinfra

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,16 +5,14 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-VAGRANT_BINARY_PATH = '/usr/local/bin/vagrant'
-
 
 def test_vagrant_binary_exists(host):
-    host.file('VAGRANT_BINARY_PATH').exists
+    assert host.file('/usr/local/bin/vagrant').exists
 
 
 def test_vagrant_binary_file(host):
-    host.file('VAGRANT_BINARY_PATH').is_file
+    assert host.file('/usr/local/bin/vagrant').is_file
 
 
-def test_vagrant_binary_run(host):
-    host.check_output('whereis vagrant') == VAGRANT_BINARY_PATH
+def test_vagrant_binary_which(host):
+    assert host.check_output('which vagrant') == '/usr/local/bin/vagrant'
